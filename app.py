67088a6d4a583bad3f05b6e169decb401797ed1a
agyeievans <evanskofiagyei06@gmail.com>
2023-07-27 16:01:46 +0000

    added footer

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ JOBS = [
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html', jobs=JOBS, company_name="Evans")
+    return render_template('home.html', jobs=JOBS, company_name="Jovian")
 
 @app.route("/api/jobs")
 def list_jobs():
