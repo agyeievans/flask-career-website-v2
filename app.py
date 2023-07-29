052908ab8cb8c0cdcf50da9a2221675b40e36ce5
agyeievans <evanskofiagyei06@gmail.com>
2023-07-29 13:30:21 +0000

    fetch and display list of jobs from database

--- app.py
+++ app.py
@@ -1,38 +1,13 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        "id": 1,
-        "title": "Data Analyst",
-        "location": "Accra, Ghana",
-        "salary": "GHS 4,000.00"
-    },
-    {
-        "id": 2,
-        "title": "Data Scientist",
-        "location": "Kumasi, Ghana",
-        "salary": "GHS 5,000.00"
-    },
-    {
-        "id": 3,
-        "title": "Web Developer",
-        "location": "Accra, Ghana",
-        
-    },
-    {
-        "id": 4,
-        "title": "Backend Enginner",
-        "location": "Remote",
-        "salary": "USD 70,000.00"
-    },
-]
-
 @app.route("/")
 def home():
-    return render_template('home.html', jobs=JOBS, company_name="Jovian")
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs, company_name="Jovian")
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    return jsonify(load_jobs_from_db)
