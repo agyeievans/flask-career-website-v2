10fb9d2c2db5e773df2195060694cff6e2097a2f
agyeievans <evanskofiagyei06@gmail.com>
2023-07-30 15:17:47 +0000

    create and store job applications in database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -22,5 +22,12 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   
-  return render_template('jobpage.html', 
-                         job=job)
+  return render_template('jobpage.html', job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+
+    add_application_to_db(id, data)
+    return render_template('application_submitted.html', application=data, job=job)
